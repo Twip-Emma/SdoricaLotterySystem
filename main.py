4da2f1c5d935a3f209f47e78494bd311f7f68c2e
七画一只妖 <1157529280@qq.com>
2023-11-14 11:42:00 +0800

    新增功能：全图鉴查看，优化代码

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Author: 七画一只妖 1157529280@qq.com
 Date: 2023-11-10 10:21:50
 LastEditors: 七画一只妖 1157529280@qq.com
-LastEditTime: 2023-11-10 21:51:51
+LastEditTime: 2023-11-14 11:31:03
 '''
 import uuid
 import time
@@ -12,8 +12,11 @@ import asyncio
 loop = asyncio.get_event_loop()
 
 
-# 万象十连
+# 用户进行十连
 # print(loop.run_until_complete(get_drow.drow("123")))
 
-# 万象图鉴
+# 用户的图鉴
 print(loop.run_until_complete(user_ill.get_user_ill("123")))
+
+# 查看完整图鉴
+# print(loop.run_until_complete(user_ill.get_pool_ill()))
