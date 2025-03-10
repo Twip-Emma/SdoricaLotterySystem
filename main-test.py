8056141143bafd170072ab71acd5f5053bd90d01
tanyongqiang <1157529280@qq.com>
2025-03-10 22:25:49 +0800

    更新人像

--- main-test.py
+++ main-test.py
@@ -2,9 +2,10 @@
 Author: 七画一只妖 1157529280@qq.com
 Date: 2023-11-10 10:21:50
 LastEditors: tanyongqiang 1157529280@qq.com
-LastEditTime: 2025-03-08 18:17:38
+LastEditTime: 2025-03-10 22:20:51
 '''
 from payload import get_drow, user_ill
+import datetime
 
 import asyncio
 loop = asyncio.get_event_loop()
@@ -23,4 +24,6 @@ loop = asyncio.get_event_loop()
 # print(loop.run_until_complete(user_ill.get_user_train('123')))
 
 # 查看所有用户练度排行
-print(loop.run_until_complete(user_ill.get_all_rank()))
+# print(loop.run_until_complete(user_ill.get_all_rank()))
+
+# print(datetime.datetime.now().strftime("%Y-%m-%d"))
